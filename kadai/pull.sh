85496ebed54018cac8de0ab52951ac8e65e62426
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-05-13 20:57:15 +0900

    pull前にcleanを実行

--- kadai/pull.sh
+++ kadai/pull.sh
@@ -1,105 +1,140 @@
 cd n19001
+git clean -f
 git pull
 cd ..
 cd n19002
+git clean -f
 git pull
 cd ..
 cd n19003
+git clean -f
 git pull
 cd ..
 cd n19004
+git clean -f
 git pull
 cd ..
 cd n19005
+git clean -f
 git pull
 cd ..
 cd n19006
+git clean -f
 git pull
 cd ..
 cd n19007
+git clean -f
 git pull
 cd ..
 cd n19008
+git clean -f
 git pull
 cd ..
 cd n19010
+git clean -f
 git pull
 cd ..
 cd n19011
+git clean -f
 git pull
 cd ..
 cd n19012
+git clean -f
 git pull
 cd ..
 cd n19013
+git clean -f
 git pull
 cd ..
 cd n19015
+git clean -f
 git pull
 cd ..
 cd n19016
+git clean -f
 git pull
 cd ..
 cd n19017
+git clean -f
 git pull
 cd ..
 cd n19018
+git clean -f
 git pull
 cd ..
 cd s19001
+git clean -f
 git pull
 cd ..
 cd s19002
+git clean -f
 git pull
 cd ..
 cd s19003
+git clean -f
 git pull
 cd ..
 cd s19004
+git clean -f
 git pull
 cd ..
 cd s19005
+git clean -f
 git pull
 cd ..
 cd s19006
+git clean -f
 git pull
 cd ..
 cd s19007
+git clean -f
 git pull
 cd ..
 cd s19008
+git clean -f
 git pull
 cd ..
 cd s19009
+git clean -f
 git pull
 cd ..
 cd s19010
+git clean -f
 git pull
 cd ..
 cd s19011
+git clean -f
 git pull
 cd ..
 cd s19012
+git clean -f
 git pull
 cd ..
 cd s19013
+git clean -f
 git pull
 cd ..
 cd s19014
+git clean -f
 git pull
 cd ..
 cd s19015
+git clean -f
 git pull
 cd ..
 cd s19016
+git clean -f
 git pull
 cd ..
 cd s19017
+git clean -f
 git pull
 cd ..
 cd s19018
+git clean -f
 git pull
 cd ..
 cd s19019
+git clean -f
 git pull
 cd ..
