ddb814279789659f4f98df72cc20b2032bc6dc94
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-05-28 09:16:58 +0900

    pullする前に学籍番号表示

--- kadai/pullall.sh
+++ kadai/pullall.sh
@@ -1,140 +1,175 @@
 cd n19001
+echo n19001
 git clean -f
 git pull
 cd ..
 cd n19002
+echo n19002
 git clean -f
 git pull
 cd ..
 cd n19003
+echo n19003
 git clean -f
 git pull
 cd ..
 cd n19004
+echo n19004
 git clean -f
 git pull
 cd ..
 cd n19005
+echo n19005
 git clean -f
 git pull
 cd ..
 cd n19006
+echo n19006
 git clean -f
 git pull
 cd ..
 cd n19007
+echo n19007
 git clean -f
 git pull
 cd ..
 cd n19008
+echo n19008
 git clean -f
 git pull
 cd ..
 cd n19010
+echo n19010
 git clean -f
 git pull
 cd ..
 cd n19011
+echo n19011
 git clean -f
 git pull
 cd ..
 cd n19012
+echo n19012
 git clean -f
 git pull
 cd ..
 cd n19013
+echo n19013
 git clean -f
 git pull
 cd ..
 cd n19015
+echo n19015
 git clean -f
 git pull
 cd ..
 cd n19016
+echo n19016
 git clean -f
 git pull
 cd ..
 cd n19017
+echo n19017
 git clean -f
 git pull
 cd ..
 cd n19018
+echo n19018
 git clean -f
 git pull
 cd ..
 cd s19001
+echo s19001
 git clean -f
 git pull
 cd ..
 cd s19002
+echo s19002
 git clean -f
 git pull
 cd ..
 cd s19003
+echo s19003
 git clean -f
 git pull
 cd ..
 cd s19004
+echo s19004
 git clean -f
 git pull
 cd ..
 cd s19005
+echo s19005
 git clean -f
 git pull
 cd ..
 cd s19006
+echo s19006
 git clean -f
 git pull
 cd ..
 cd s19007
+echo s19007
 git clean -f
 git pull
 cd ..
 cd s19008
+echo s19008
 git clean -f
 git pull
 cd ..
 cd s19009
+echo s19009
 git clean -f
 git pull
 cd ..
 cd s19010
+echo s19010
 git clean -f
 git pull
 cd ..
 cd s19011
+echo s19011
 git clean -f
 git pull
 cd ..
 cd s19012
+echo s19012
 git clean -f
 git pull
 cd ..
 cd s19013
+echo s19013
 git clean -f
 git pull
 cd ..
 cd s19014
+echo s19014
 git clean -f
 git pull
 cd ..
 cd s19015
+echo s19015
 git clean -f
 git pull
 cd ..
 cd s19016
+echo s19016
 git clean -f
 git pull
 cd ..
 cd s19017
+echo s19017
 git clean -f
 git pull
 cd ..
 cd s19018
+echo s19018
 git clean -f
 git pull
 cd ..
 cd s19019
+echo s19019
 git clean -f
 git pull
 cd ..
